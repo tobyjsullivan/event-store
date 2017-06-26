c6e67fb804d09dfba60d7a33c8d5f4e5f332ea3a
Toby Sullivan <tobyjsullivan@gmail.com>
2017-06-26 08:04:07 -0700

    Save events to S3

--- app.go
+++ app.go
@@ -8,7 +8,50 @@ import (
 
     "github.com/urfave/negroni"
     "github.com/gorilla/mux"
+    "github.com/tobyjsullivan/event-store.v3/events"
+    "encoding/base64"
+    "github.com/tobyjsullivan/event-store.v3/store"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
+    "log"
 )
+
+var (
+    eventStore *store.Store
+)
+
+func init() {
+    bucket := os.Getenv("S3_BUCKET")
+    if bucket == "" {
+        panic("S3_BUCKET must be set.")
+    }
+    region := os.Getenv("AWS_REGION")
+    if os.Getenv("AWS_REGION") == "" {
+        panic("AWS_REGION must be set.")
+    }
+    if os.Getenv("AWS_ACCESS_KEY_ID") == "" {
+        panic("AWS_ACCESS_KEY_ID must be set.")
+    }
+    if os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+        panic("AWS_SECRET_ACCESS_KEY must be set.")
+    }
+
+    logger := log.New(os.Stdout, "[svc] ", 0)
+
+    sess := session.Must(session.NewSession(
+        &aws.Config{
+            Credentials: credentials.NewEnvCredentials(),
+            Region: aws.String(region),
+            Logger: aws.LoggerFunc(logger.Println),
+            LogLevel: aws.LogLevel(aws.LogDebugWithHTTPBody),
+        },
+    ))
+    s3svc := s3.New(sess)
+
+    eventStore = store.NewS3Store(s3svc, bucket)
+}
 
 func main() {
     r := buildRoutes()
@@ -26,12 +69,65 @@ func main() {
 
 func buildRoutes() http.Handler {
     r := mux.NewRouter()
-    r.HandleFunc("/", statusHandler).
-            Methods("GET")
+    r.HandleFunc("/", statusHandler).Methods("GET")
+    r.HandleFunc("/events", addEventHandler).Methods("POST")
 
     return r
 }
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "The service is online!\n")
+}
+
+func addEventHandler(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseForm()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    f := r.Form
+    prevIdHex := f.Get("previous")
+    eventType := f.Get("type")
+    dataBase64 := f.Get("data")
+
+    if prevIdHex == "" {
+        http.Error(w, "Previous event ID is required.", http.StatusBadRequest)
+        return
+    }
+    if eventType == "" {
+        http.Error(w, "Event type is required.", http.StatusBadRequest)
+        return
+    }
+    if dataBase64 == "" {
+        http.Error(w, "Data is required.", http.StatusBadRequest)
+        return
+    }
+
+    prevId := events.NewEventID()
+    err = prevId.Parse(prevIdHex)
+    if err != nil {
+        http.Error(w, "Error parsing previous ID: "+err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    data, err := base64.StdEncoding.DecodeString(dataBase64)
+    if err != nil {
+        http.Error(w, "Error parsing data: "+err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    e := &events.Event{
+        PreviousEvent: prevId,
+        Type: eventType,
+        Data: data,
+    }
+    err = eventStore.Save(e)
+    if err != nil {
+        http.Error(w, "Error saving event: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    id := e.ID()
+    fmt.Fprintf(w, "Event saved: %s", id.String())
 }
--- store/store.go
+++ store/store.go
@@ -1,1 +1,57 @@
 package store
+
+import (
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/tobyjsullivan/event-store.v3/events"
+    "github.com/aws/aws-sdk-go/aws"
+    "encoding/base64"
+    "bytes"
+    "encoding/json"
+)
+
+type Store struct {
+    s3svc *s3.S3
+    bucket string
+}
+
+func NewS3Store(svc *s3.S3, bucket string) *Store {
+    return &Store{
+        s3svc: svc,
+        bucket: bucket,
+    }
+}
+
+type eventFormat struct {
+    Prev string `json:"prevous"`
+    Type string `json:"type"`
+    Data string `json:"data"`
+}
+
+func (s *Store) Save(e *events.Event) error {
+    id := e.ID()
+    key := id.String()
+
+    content := &eventFormat{
+        Prev: e.PreviousEvent.String(),
+        Type: e.Type,
+        Data: base64.StdEncoding.EncodeToString(e.Data),
+    }
+
+    var buf bytes.Buffer
+    encoder := json.NewEncoder(&buf)
+    err := encoder.Encode(content)
+    if err != nil {
+        return err
+    }
+
+    _, err = s.s3svc.PutObject(&s3.PutObjectInput{
+        Body: bytes.NewReader(buf.Bytes()),
+        Bucket: aws.String(s.bucket),
+        Key: aws.String(key),
+    })
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
